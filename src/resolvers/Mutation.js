7a33ebbac1a4b4987d563189f7e7fd3b8969d691
Mehrad Rousta <mehrad77@gmail.com>
2018-12-18 19:09:02 +0330

    add "createPodcastStation" mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,32 @@
-const Mutations = {
+const db = require('../db');
 
+const Mutations = {
+    async createPodcastStation(parent, args, ctx, info) {
+        // 1. verify if user is Logged In
+        // if(!ctx.request.userId){
+        //   throw new Error("You must be logged to do that!");
+        // }
+
+        // 2. verify if user has "PODCASTADD" permission
+        // TODO
+
+        // 3. add podcast
+        const podcastStation = await db.mutation.createPodcastStation(
+          {
+            data: {
+                // 4. create a relationship between the PodcastStation and the User
+                // user: {
+                //     connect: {
+                //         id: ctx.request.userId,
+                //     },
+                // },
+              ...args,
+            },
+          },
+          info
+        );
+        return podcastStation;
+      },
 };
 
 module.exports = Mutations;
