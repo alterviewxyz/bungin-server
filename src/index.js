51b92bbfe519638438182224c978511c531d9307
Mehrad Rousta <mehrad77@gmail.com>
2018-12-22 05:49:36 +0330

    add cookieParser and decode JWT to get user Id from cockies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
+const cookieParser = require('cookie-parser');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser());
+
+// 1. decode JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  
+  const { token } = req.cookies;
+  
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId into the req for future requests to access
+    req.userId = userId;
+  }
+  
+  next();
+});
+
 // TODO Use express middlware to populate current user
 
 server.start(
